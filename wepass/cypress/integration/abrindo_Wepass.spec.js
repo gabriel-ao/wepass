4b5d071de4ae8900ab56ddba53768d4e75069eda
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-11-04 22:20:23 -0300

    comecando entender de cypress

---
+++ wepass/cypress/integration/abrindo_Wepass.spec.js
@@ -0,0 +1,9 @@
+describe("Deve abrir a tela de apresentação do wepass", () => {
+  it("Abrir tela!", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/");
+    });
+
+    // expect(true).to.equal(true);
+  });
+});
